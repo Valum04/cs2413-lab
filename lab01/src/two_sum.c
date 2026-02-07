16949323ab80d3649ad83e2deb688a75b03053df
Valum04 <mdegroot3@yahoo.com>
2026-02-07 17:59:23 -0600

    lab01 updated

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -1,13 +1,32 @@
 #include "two_sum.h"
 
-int two_sum(const int* nums, size_t n, int target, size_t* out_i, size_t* out_j) {
+int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
 
- // TODO: implement Two Sum.
+    // TODO: implement Two Sum.
     // Requirements:
     //  - find i < j such that nums[i] + nums[j] == target
     //  - store indices in *out_i and *out_j
     //  - return 1 if found; otherwise return 0
 
+
+    int count = 0;
+    
+
+    for(int i = 0; i < n -1; i++)
+    {
+        for(int j = 1; j < n; j++) 
+        {
+            if(nums[i] + nums[j] == target)
+        {
+            *out_i = i; 
+            *out_j = j;
+            return 1;
+        }
+        }
+    }
+
+
     return 0;
 }
 
+//think about the time complexity and space complexity of your solution
