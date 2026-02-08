f89f8013fddf9dccf12421ab5737a596c9f571d2
Valum04 <mdegroot3@yahoo.com>
2026-02-07 18:08:18 -0600

    Updated lab01

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -3,6 +3,28 @@
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
     
     // implement your code here
+//nums[i] + nums[j] + nums[k] == 0
+int target = 0;
+   int i = 0;
+    int j = i + 1;
+    int k = n - 1;
+    for(; i < n; i++)
+    {
+        if(!(i < j && j < k))
+            return 0;
+        if(nums[i] + nums[j] + nums[k] < target)
+            j++;
+        else if (nums[i] + nums[j] + nums[k] > target)
+            k--;
+        else if (nums[i] + nums[j] + nums[k] == target)
+        {
+            *out_i = i; 
+            *out_j = j;
+            *out_k = k; 
+            
+            return 1;
+        }
+    }
 
     return 0;
 }
